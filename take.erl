dcdc8b1277980274af79dbdb8603788a24d11df1
Milton Mazzarri <milmazz@gmail.com>
2017-03-15 15:12:15 -0500

    Do lists:reverse at the end

--- take.erl
+++ take.erl
@@ -1,5 +1,5 @@
 -module(take).
--export([take/2, take_t/2]).
+-export([take_d/2, take_t/2]).
 -include_lib("eunit/include/eunit.hrl").
 
 %%%%%%%%
@@ -8,38 +8,6 @@
 
 % Define a function take that takes the first N elements from a list.
 
-% Step-by-step evaluation (direct)
-%
-% take(4, "hello")
-% "h" ++ take(4, "ello", 1)
-% "h" ++ "e" ++ take(4, "llo", 2)
-% "h" ++ "e" ++ "l" ++ take(4, "lo", 3)
-% "h" ++ "e" ++ "l" ++ "l" ++ take(4, "o", 4)
-% "h" ++ "e" ++ "l" ++ "l" ++ []
-% "hell"
-
--spec take(integer(), [T]) -> [T].
-take(_, []) ->
-  [];
-take(0, [_X|_Xs]) ->
-  [];
-take(N, [X|Xs]) when is_integer(N), N > 0 ->
-  [X] ++ take(N, Xs, 1).
-
-take(_N, [], _Acc) ->
-  [];
-take(N, [_X|_Xs], Acc) when Acc >= N ->
-  [];
-take(N, [X|Xs], Acc) ->
-  [X] ++ take(N, Xs, Acc + 1).
-
-take_test() ->
-  ?assertEqual([], take(15, [])),
-  ?assertEqual([], take(0, "hello")),
-  ?assertEqual("hell", take(4, "hello")),
-  ?assertEqual("hello", take(5, "hello")),
-  ?assertEqual("hello", take(9, "hello")).
-
 % Step-by-step evaluation (tail)
 %
 % take_t(4, "hello")
@@ -47,7 +15,7 @@ take_test() ->
 % take_t(4, "llo", 2, "he")
 % take_t(4, "lo", 3, "hel")
 % take_t(4, "o", 4, "hell")
-% hell
+% "hell"
 
 -spec take_t(integer(), [T]) -> [T].
 take_t(_, []) ->
@@ -58,11 +26,11 @@ take_t(N, [X|Xs]) when is_integer(N), N > 0 ->
   take_t(N, Xs, 1, [X]).
 
 take_t(_N, [], _Acc, Res) ->
-  Res;
+  lists:reverse(Res);
 take_t(N, [_X|_Xs], Acc, Res) when Acc >= N ->
-  Res;
+  lists:reverse(Res);
 take_t(N, [X|Xs], Acc, Res) ->
-  take_t(N, Xs, Acc + 1, Res ++ [X]).
+  take_t(N, Xs, Acc + 1, [X|Res]).
 
 take_t_test() ->
   ?assertEqual([], take_t(15, [])),
@@ -70,3 +38,35 @@ take_t_test() ->
   ?assertEqual("hell", take_t(4, "hello")),
   ?assertEqual("hello", take_t(5, "hello")),
   ?assertEqual("hello", take_t(9, "hello")).
+
+% Step-by-step evaluation (direct)
+%
+% take_d(4, "hello")
+% "h" ++ take_d(4, "ello", 1)
+% "h" ++ "e" ++ take_d(4, "llo", 2)
+% "h" ++ "e" ++ "l" ++ take_d(4, "lo", 3)
+% "h" ++ "e" ++ "l" ++ "l" ++ take_d(4, "o", 4)
+% "h" ++ "e" ++ "l" ++ "l" ++ []
+% "hell"
+
+-spec take_d(integer(), [T]) -> [T].
+take_d(_, []) ->
+  [];
+take_d(0, [_X|_Xs]) ->
+  [];
+take_d(N, [X|Xs]) when is_integer(N), N > 0 ->
+  [X] ++ take_d(N, Xs, 1).
+
+take_d(_N, [], _Acc) ->
+  [];
+take_d(N, [_X|_Xs], Acc) when Acc >= N ->
+  [];
+take_d(N, [X|Xs], Acc) ->
+  [X] ++ take_d(N, Xs, Acc + 1).
+
+take_d_test() ->
+  ?assertEqual([], take_d(15, [])),
+  ?assertEqual([], take_d(0, "hello")),
+  ?assertEqual("hell", take_d(4, "hello")),
+  ?assertEqual("hello", take_d(5, "hello")),
+  ?assertEqual("hello", take_d(9, "hello")).
