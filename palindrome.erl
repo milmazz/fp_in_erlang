9fde9ed9409753da34d469d057e07fdf580a8df6
Milton Mazzarri <milmazz@gmail.com>
2017-03-15 23:13:20 -0500

    add palindrome/1

---
+++ palindrome.erl
@@ -0,0 +1,21 @@
+-module(palindrome).
+-export([palindrome/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+%%%%%%%%
+% 2.15 %
+%%%%%%%%
+
+% Define a function palindrome, that returns true or false depending on whether
+% the list is a palindrome - the same when read right to left as when read left
+% to right.
+
+-spec palindrome(string()) -> boolean().
+palindrome(X) ->
+  Y = re:replace(string:to_lower(X), "[\s']", "", [global,{return,list}]),
+  string:equal(Y, lists:reverse(Y)).
+
+palidrome_test() ->
+  ?assertEqual(true, palindrome("Madam I\'m Adam")),
+  ?assertEqual(false, palindrome("Arepa")).
+
